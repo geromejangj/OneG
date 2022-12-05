ec6d20d635d54b33a7fd40e20b7926ceb8419975
geromejangj <geromeldolorfino@su.edu.ph>
2022-12-05 14:06:46 +0800

    Adjustments

--- OneG/masterKey.Designer.vb
+++ OneG/masterKey.Designer.vb
@@ -31,10 +31,10 @@ Partial Class masterKey
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(45, 67)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(43, 79)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(396, 42)
+        Me.Label1.Size = New System.Drawing.Size(366, 39)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "INPUT MASTER KEY"
         '
